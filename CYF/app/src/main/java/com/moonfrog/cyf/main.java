23b319ffadf9af826b27c9e171bc418240a58ca4
Srinath Ramanujan <gr8srinath@gmail.com>
2015-03-30 19:22:09 +0530

    updated UI

--- CYF/app/src/main/java/com/moonfrog/cyf/main.java
+++ CYF/app/src/main/java/com/moonfrog/cyf/main.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
@@ -20,7 +24,7 @@ public class main extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
