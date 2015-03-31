90f4a42ef30597558d5a8ac7dab233686d17ff6a
Srinath Ramanujan <gr8srinath@gmail.com>
2015-03-31 16:00:12 +0530

    Added countries list

--- CYF/app/src/main/java/com/moonfrog/cyf/HangmanChallengeFragment.java
+++ CYF/app/src/main/java/com/moonfrog/cyf/HangmanChallengeFragment.java
@@ -21,9 +21,16 @@ import com.facebook.messenger.MessengerUtils;
 import com.facebook.messenger.ShareToMessengerParams;
 import com.moonfrog.cyf.view.SlidingTabLayout;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 public class HangmanChallengeFragment extends Fragment {
@@ -36,6 +43,16 @@ public class HangmanChallengeFragment extends Fragment {
             "Sports",
             "Games"
     };
+    public String[][] category_word_list = {
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+    };
+
+    public JSONObject word_list = null;
 
     private SlidingTabLayout mSlidingTabLayout;
     private ViewPager mViewPager;
@@ -47,6 +64,35 @@ public class HangmanChallengeFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        try {
+            InputStream is = getResources().openRawResource(R.raw.word_list);
+            Writer writer = new StringWriter();
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                is.close();
+            }
+            word_list = new JSONObject(writer.toString());
+
+            for(int i = 0 ; i < categories.length; i++) {
+                JSONArray list = word_list.getJSONArray(categories[i]);
+                String[] strList = new String[list.length()];
+                for(int j = 0, count = list.length(); j < count; j++) {
+                    strList[j] = list.getString(j);
+                }
+                category_word_list[i] = strList;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
         mViewPager.setAdapter(new HangmanChallengePageAdapter());
 
@@ -71,28 +117,29 @@ public class HangmanChallengeFragment extends Fragment {
         }
 
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
+        public Object instantiateItem(ViewGroup container, final int position) {
             // Inflate a new layout from our resources
             View view = getActivity().getLayoutInflater().inflate(R.layout.hangman_challenge_pager_item, container, false);
             container.addView(view);
 
             ListView listView = (ListView) view.findViewById(R.id.hangman_category_select);
-            final String[][] challenges_icons = {
-                    {"Word 1", ""},
-                    {"Word 2", ""},
-                    {"Word 3", ""},
-                    {"Word 4", ""},
-                    {"Word U", ""},
-                    {"Word V", ""},
-                    {"Word W", ""},
-                    {"Word X", ""},
-                    {"Word Y", ""},
-                    {"Word Z", ""}
-            };
+
+//            final String[][] challenges_icons = {
+//                    {"Word 1", ""},
+//                    {"Word 2", ""},
+//                    {"Word 3", ""},
+//                    {"Word 4", ""},
+//                    {"Word U", ""},
+//                    {"Word V", ""},
+//                    {"Word W", ""},
+//                    {"Word X", ""},
+//                    {"Word Y", ""},
+//                    {"Word Z", ""}
+//            };
             ArrayList<ListAdapter.ListElement> sliderMenu = new ArrayList<>();
 
-            for (String s[] : challenges_icons) {
-                sliderMenu.add(new ListAdapter.ListElement(s[0], s[1]));
+            for (String s : category_word_list[position]) {
+                sliderMenu.add(new ListAdapter.ListElement(s, ""));
             }
 
             ListAdapter mAdapter = new ListAdapter(view.getContext(), sliderMenu);
@@ -100,8 +147,8 @@ public class HangmanChallengeFragment extends Fragment {
 
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    String shaHash = AESEncryption.encrypt(challenges_icons[position][0]);
+                public void onItemClick(AdapterView<?> parent, View view, int inner_position, long id) {
+                    String shaHash = AESEncryption.encrypt(category_word_list[position][inner_position]);
                     Log.e("encrypted key ", shaHash);
 
                     Uri contentUri = Uri.fromFile(new File("/sdcard/Downloads/img.jpg"));
